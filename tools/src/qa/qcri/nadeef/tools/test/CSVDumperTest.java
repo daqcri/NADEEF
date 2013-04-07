57bfe882e71282809ff7c08f2e8b17954877cd73
siyin <siyin@79f9f751-2b77-4afd-85d4-486db02d7cbf>
2013-04-07 03:49:34 +0000

    CSVDumper.

---
+++ tools/src/qa/qcri/nadeef/tools/test/CSVDumperTest.java
@@ -0,0 +1,12 @@
+
+package qa.qcri.nadeef.tools.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: si
+ * Date: 4/4/13
+ * Time: 9:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CSVDumperTest {
+}
