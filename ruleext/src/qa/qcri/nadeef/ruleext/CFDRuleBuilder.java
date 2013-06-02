de9e03a9aab12821d14a06808c76503be33dd2fe
aebaid <aebaid@79f9f751-2b77-4afd-85d4-486db02d7cbf>
2013-06-02 07:48:33 +0000

    CFD default name

--- ruleext/src/qa/qcri/nadeef/ruleext/CFDRuleBuilder.java
+++ ruleext/src/qa/qcri/nadeef/ruleext/CFDRuleBuilder.java
@@ -5,242 +5,255 @@
 
 package qa.qcri.nadeef.ruleext;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupFile;
+
 import qa.qcri.nadeef.core.datamodel.Column;
 import qa.qcri.nadeef.core.datamodel.SimpleExpression;
 import qa.qcri.nadeef.core.util.RuleBuilder;
 import qa.qcri.nadeef.tools.CommonTools;
 
-import java.io.File;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.List;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 /**
  * Template engine for CFD rule.
- *
+ * 
+ * @author Amr Ebaid (aebaid@qf.org.qa)
+ * @author Si Yin (siyin@qf.org.qa)
  */
 public class CFDRuleBuilder extends RuleBuilder {
-	protected List<String> lhs;
-	protected List<String> rhs;
-	protected List<List<SimpleExpression>> filterExpressions;
-	protected STGroupFile singleSTGroup;
-	protected STGroupFile pairSTGroup;
+    protected List<String>                 lhs;
+    protected List<String>                 rhs;
+    protected List<List<SimpleExpression>> filterExpressions;
+    protected STGroupFile                  singleSTGroup;
+    protected STGroupFile                  pairSTGroup;
 
-	/**
-	 * Generates and compiles the rule .class file without loading it.
-	 * 
-	 * @return Output class file.
-	 */
-	@Override
-	public Collection<File> compile() throws Exception {
-		List<File> result = Lists.newArrayList();
-		singleSTGroup = new STGroupFile(
-			"qa/qcri/nadeef/ruleext/template/SingleCFDRuleBuilder.stg", '$', '$');
-		pairSTGroup = new STGroupFile(
-			"qa/qcri/nadeef/ruleext/template/PairCFDRuleBuilder.stg", '$', '$');
+    /**
+     * Generates and compiles the rule .class file without loading it.
+     * 
+     * @return Output class file.
+     */
+    @Override
+    public Collection<File> compile() throws Exception {
+        List<File> result = Lists.newArrayList();
+        singleSTGroup = new STGroupFile(
+                "qa/qcri/nadeef/ruleext/template/SingleCFDRuleBuilder.stg",
+                '$', '$');
+        pairSTGroup = new STGroupFile(
+                "qa/qcri/nadeef/ruleext/template/PairCFDRuleBuilder.stg", '$',
+                '$');
 
-		ST sst = singleSTGroup.getInstanceOf("cfdTemplate");
-		ST pst = pairSTGroup.getInstanceOf("cfdTemplate");
-		ST targetST;
+        ST sst = singleSTGroup.getInstanceOf("cfdTemplate");
+        ST pst = pairSTGroup.getInstanceOf("cfdTemplate");
+        ST targetST;
 
-		sst.add("leftHandSide", lhs);
-		pst.add("leftHandSide", lhs);
+        sst.add("leftHandSide", lhs);
+        pst.add("leftHandSide", lhs);
 
-		for (int i = 0; i < filterExpressions.size(); i++) {
-			List<SimpleExpression> filters = filterExpressions.get(i);
-			ST leftExpressionST = singleSTGroup.getInstanceOf("lExpressionItem");
-			List<String> leftExpressions = Lists.newArrayList();
+        for (int i = 0; i < filterExpressions.size(); i++) {
+            List<SimpleExpression> filters = filterExpressions.get(i);
+            ST leftExpressionST = singleSTGroup
+                    .getInstanceOf("lExpressionItem");
+            List<String> leftExpressions = Lists.newArrayList();
 
-			// Pass over items of lhs
-			for (int j = 0; j < lhs.size(); j++) {
-				SimpleExpression expression = filters.get(j);
-				String eValue = expression.getValue();
-				if (!eValue.equals("_")) {
-					leftExpressionST.add("columnName", expression.getLeft().getFullAttributeName());
-					leftExpressionST.add("value", eValue);
-					leftExpressions.add(leftExpressionST.render());
-					leftExpressionST.remove("columnName");
-					leftExpressionST.remove("value");
-				}
-			}
+            // Pass over items of lhs
+            for (int j = 0; j < lhs.size(); j++) {
+                SimpleExpression expression = filters.get(j);
+                String eValue = expression.getValue();
+                if (!eValue.equals("_")) {
+                    leftExpressionST.add("columnName", expression.getLeft()
+                            .getFullAttributeName());
+                    leftExpressionST.add("value", eValue);
+                    leftExpressions.add(leftExpressionST.render());
+                    leftExpressionST.remove("columnName");
+                    leftExpressionST.remove("value");
+                }
+            }
 
-			// Normalize CFD; create a file for each column in RHS
-			for (int j = 0; j < rhs.size(); j++) {
-				String rhsCol = rhs.get(j);
+            // Normalize CFD; create a file for each column in RHS
+            for (int j = 0; j < rhs.size(); j++) {
+                String rhsCol = rhs.get(j);
 
-				SimpleExpression expression = filters.get(j + lhs.size());
-				String eValue = expression.getValue();
-				if (!eValue.equals("_")) {
-					ST rightExpressionST = singleSTGroup.getInstanceOf("rExpressionItem");
-					rightExpressionST.add(
-                        "columnName", expression.getLeft().getFullAttributeName()
-                    );
-					rightExpressionST.add("value", eValue);
-					List<String> rightExpressions = Lists.newArrayList();
-					rightExpressions.add(rightExpressionST.render());
-					rightExpressionST.remove("columnName");
-					rightExpressionST.remove("value");
+                SimpleExpression expression = filters.get(j + lhs.size());
+                String eValue = expression.getValue();
+                if (!eValue.equals("_")) {
+                    ST rightExpressionST = singleSTGroup
+                            .getInstanceOf("rExpressionItem");
+                    rightExpressionST.add("columnName", expression.getLeft()
+                            .getFullAttributeName());
+                    rightExpressionST.add("value", eValue);
+                    List<String> rightExpressions = Lists.newArrayList();
+                    rightExpressions.add(rightExpressionST.render());
+                    rightExpressionST.remove("columnName");
+                    rightExpressionST.remove("value");
 
-					sst.add("lExpression", leftExpressions);
-					sst.add("rExpression", rightExpressions);
-					targetST = sst;
-				} else {
-					pst.add("lExpression", leftExpressions);
-					targetST = pst;
-				}
+                    sst.add("lExpression", leftExpressions);
+                    sst.add("rExpression", rightExpressions);
+                    targetST = sst;
+                } else {
+                    pst.add("lExpression", leftExpressions);
+                    targetST = pst;
+                }
 
-				targetST.add("rightHandSide", rhsCol);
+                targetST.add("rightHandSide", rhsCol);
 
-				if (Strings.isNullOrEmpty(originalRuleName)) {
-					ruleName = "DefaultCFD"
-							+ CommonTools.toHashCode(Calendar.getInstance()
-									.getTime().toString()) + "_"
-							+ rhsCol.substring(rhsCol.lastIndexOf('.') + 1)
-							+ "_" + i;
-				} else {
-					// remove all the empty spaces to make it a valid class
-					// name.
-					ruleName = originalRuleName.replace(" ", "") + "_"
-							+ rhsCol.substring(rhsCol.lastIndexOf('.') + 1)
-							+ "_" + i;
-				}
+                if (Strings.isNullOrEmpty(originalRuleName)) {
+                    ruleName = "DefaultCFD"
+                            + CommonTools.toHashCode(value.get(i + 1)) + "_"
+                            + rhsCol.substring(rhsCol.lastIndexOf('.') + 1)
+                            + "_" + i;
+                } else {
+                    // remove all the empty spaces to make it a valid class
+                    // name.
+                    ruleName = originalRuleName.replace(" ", "") + "_"
+                            + rhsCol.substring(rhsCol.lastIndexOf('.') + 1)
+                            + "_" + i;
+                }
 
-				targetST.add("CFDName", ruleName);
+                targetST.add("CFDName", ruleName);
 
-				File outputFile = getOutputFile();
-				targetST.write(outputFile, null);
+                File outputFile = getOutputFile();
+                targetST.write(outputFile, null);
 
-				String fullPath = outputFile.getAbsolutePath();
-				// skip compiling if the .class file already exists.
-				File classFile = new File(fullPath.replace(".java", ".class"));
-				if (classFile.exists() || CommonTools.compileFile(outputFile)) {
-					result.add(classFile);
-				}
+                String fullPath = outputFile.getAbsolutePath();
+                // skip compiling if the .class file already exists.
+                File classFile = new File(fullPath.replace(".java", ".class"));
+                if (classFile.exists() || CommonTools.compileFile(outputFile)) {
+                    result.add(classFile);
+                }
 
-				targetST.remove("CFDName");
-				targetST.remove("lExpression");
-				if (targetST == sst) {
-					targetST.remove("rExpression");
-				}
-				targetST.remove("rightHandSide");
-			}
-		}
+                targetST.remove("CFDName");
+                targetST.remove("lExpression");
+                if (targetST == sst) {
+                    targetST.remove("rExpression");
+                }
+                targetST.remove("rightHandSide");
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * Interpret a rule from input text stream.
-	 */
-	@Override
-	protected void parse() {
-		lhs = Lists.newArrayList();
-		rhs = Lists.newArrayList();
-		filterExpressions = Lists.newArrayList();
+    /**
+     * Interpret a rule from input text stream.
+     */
+    @Override
+    protected void parse() {
+        lhs = Lists.newArrayList();
+        rhs = Lists.newArrayList();
+        filterExpressions = Lists.newArrayList();
 
-		String head = value.get(0);
-		String[] splits = head.trim().split("\\|");
-		String split;
-		if (splits.length != 2) {
-			throw new IllegalArgumentException("Invalid rule description " + head);
-		}
+        String head = value.get(0);
+        String[] splits = head.trim().split("\\|");
+        String split;
+        if (splits.length != 2) {
+            throw new IllegalArgumentException("Invalid rule description "
+                    + head);
+        }
 
-		// parse the LHS
-		String[] lhsSplits = splits[0].split(",");
-		// use the first one as default table name.
-		String defaultTable = tableNames.get(0);
-		for (int i = 0; i < lhsSplits.length; i++) {
-			split = lhsSplits[i].trim().toLowerCase();
-			if (Strings.isNullOrEmpty(split)) {
-				throw new IllegalArgumentException("Invalid rule description " + head);
-			}
+        // parse the LHS
+        String[] lhsSplits = splits[0].split(",");
+        // use the first one as default table name.
+        String defaultTable = tableNames.get(0);
+        for (int i = 0; i < lhsSplits.length; i++) {
+            split = lhsSplits[i].trim().toLowerCase();
+            if (Strings.isNullOrEmpty(split)) {
+                throw new IllegalArgumentException("Invalid rule description "
+                        + head);
+            }
 
-			if (!Strings.isNullOrEmpty(defaultTable) && !CommonTools.isValidColumnName(split)) {
-				lhs.add(new Column(defaultTable, split).getFullAttributeName());
-			} else {
-				lhs.add(split);
-			}
-		}
+            if (!Strings.isNullOrEmpty(defaultTable)
+                    && !CommonTools.isValidColumnName(split)) {
+                lhs.add(new Column(defaultTable, split).getFullAttributeName());
+            } else {
+                lhs.add(split);
+            }
+        }
 
-		// parse the RHS
-		String[] rhsSplits = splits[1].trim().split(",");
-		for (int i = 0; i < rhsSplits.length; i++) {
-			split = rhsSplits[i].trim().toLowerCase();
-			if (split.isEmpty()) {
-				throw new IllegalArgumentException("Invalid rule description " + head);
-			}
+        // parse the RHS
+        String[] rhsSplits = splits[1].trim().split(",");
+        for (int i = 0; i < rhsSplits.length; i++) {
+            split = rhsSplits[i].trim().toLowerCase();
+            if (split.isEmpty()) {
+                throw new IllegalArgumentException("Invalid rule description "
+                        + head);
+            }
 
-			if (!Strings.isNullOrEmpty(defaultTable) && !CommonTools.isValidColumnName(split)) {
-				rhs.add(new Column(defaultTable, split).getFullAttributeName());
-			} else {
-				rhs.add(split);
-			}
-		}
+            if (!Strings.isNullOrEmpty(defaultTable)
+                    && !CommonTools.isValidColumnName(split)) {
+                rhs.add(new Column(defaultTable, split).getFullAttributeName());
+            } else {
+                rhs.add(split);
+            }
+        }
 
-		// parse condition line
-		// TODO: currently we recreate a new FDRule per condition line, but
-		// it would have more optimizations based on a buck of lines.
-		for (int i = 1; i < value.size(); i++) {
-			List<SimpleExpression> filter = Lists.newArrayList();
-			String line = value.get(i);
-			if (Strings.isNullOrEmpty(line)) {
-				continue;
-			}
+        // parse condition line
+        // TODO: currently we recreate a new FDRule per condition line, but
+        // it would have more optimizations based on a buck of lines.
+        for (int i = 1; i < value.size(); i++) {
+            List<SimpleExpression> filter = Lists.newArrayList();
+            String line = value.get(i);
+            if (Strings.isNullOrEmpty(line)) {
+                continue;
+            }
 
-			splits = line.trim().split(",");
-			if (splits.length != lhs.size() + rhs.size()) {
-				throw new IllegalArgumentException("Invalid rule description " + line);
-			}
+            splits = line.trim().split(",");
+            if (splits.length != lhs.size() + rhs.size()) {
+                throw new IllegalArgumentException("Invalid rule description "
+                        + line);
+            }
 
-			String curColumn;
-			for (int j = 0; j < splits.length; j++) {
-				split = splits[j].trim().toLowerCase();
-				if (j < lhs.size()) {
-					curColumn = lhs.get(j);
-				} else {
-					curColumn = rhs.get(j - lhs.size());
-				}
+            String curColumn;
+            for (int j = 0; j < splits.length; j++) {
+                split = splits[j].trim().toLowerCase();
+                if (j < lhs.size()) {
+                    curColumn = lhs.get(j);
+                } else {
+                    curColumn = rhs.get(j - lhs.size());
+                }
 
-				if (Strings.isNullOrEmpty(split)) {
-					throw new IllegalArgumentException("Invalid rule description " + line);
-				}
+                if (Strings.isNullOrEmpty(split)) {
+                    throw new IllegalArgumentException(
+                            "Invalid rule description " + line);
+                }
 
-				SimpleExpression newFilter =
-                    SimpleExpression.newEqual(new Column(curColumn), split);
-				filter.add(newFilter);
-			}
-			filterExpressions.add(filter);
-		}
-	}
+                SimpleExpression newFilter = SimpleExpression.newEqual(
+                        new Column(curColumn), split);
+                filter.add(newFilter);
+            }
+            filterExpressions.add(filter);
+        }
+    }
 
-	/**
-	 * Gets of LHS.
-	 * 
-	 * @return LHS.
-	 */
-	public List<String> getLhs() {
-		return lhs;
-	}
+    /**
+     * Gets of LHS.
+     * 
+     * @return LHS.
+     */
+    public List<String> getLhs() {
+        return lhs;
+    }
 
-	/**
-	 * Gets of RHS.
-	 * 
-	 * @return RHS.
-	 */
-	public List<String> getRhs() {
-		return rhs;
-	}
+    /**
+     * Gets of RHS.
+     * 
+     * @return RHS.
+     */
+    public List<String> getRhs() {
+        return rhs;
+    }
 
-	/**
-	 * Gets of exists filter expressions.
-	 * 
-	 * @return filter expressions.
-	 */
-	public List<List<SimpleExpression>> getFilterExpressions() {
-		return filterExpressions;
-	}
+    /**
+     * Gets of exists filter expressions.
+     * 
+     * @return filter expressions.
+     */
+    public List<List<SimpleExpression>> getFilterExpressions() {
+        return filterExpressions;
+    }
 }
