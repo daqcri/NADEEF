821e47a6f73a821d1a0d0730fd36ba7d9ec99619
QCRI <ntang@qf.org.qa>
2013-09-19 11:28:07 +0300

    Update nadeef.bat

--- nadeef.bat
+++ nadeef.bat
@@ -14,12 +14,12 @@ set BuildVersion=1.0.1019
 set argC=0
 for %%x in (%*) do Set /A argC+=1
 
-if not argC == 1 (
+if not %argC% == 1 (
     goto usage
 ) else (
     if "%1" == "console" (
-        "%JAVA_HOME%\bin\java" -d64 -Dderby.drda.startNetworkServer=true -cp out\bin\*;out\test;examples\ qa.qcri.nadeef.console.Console"
-    ) elif "%1" == "dashboard" (
+        "%JAVA_HOME%\bin\java -d64 -Dderby.drda.startNetworkServer=true -cp out\bin\*;out\test;examples\ qa.qcri.nadeef.console.Console"
+    ) else if "%1" == "dashboard" (
         "%JAVA_HOME%\bin\java -d64 -cp out/bin/*:. qa.qcri.nadeef.web.NadeefStart"
     ) else (
         goto usage
@@ -36,9 +36,9 @@ goto end
 echo Nadeef is not compiled yet. Run 'ant' to compile Nadeef.
 
 :usage
-echo 'Usage: nadeef.sh [OPTIONS]'
-echo 'Options are:'
-echo '    console : start the NADEEF console.'
-echo '    dashboard : start the NADEEF dashboard.'
+echo Usage: nadeef.sh [OPTIONS]
+echo Options are:
+echo     console : start the NADEEF console.
+echo     dashboard : start the NADEEF dashboard.
 
 :end
