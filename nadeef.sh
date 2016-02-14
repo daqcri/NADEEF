c8543cf843f5d71e01fb0d6f780e3108d9120dad
Hossam Hammady <github@hammady.net>
2016-02-14 12:37:36 +0300

    Abstracting nadeef.sh tasks and considering JAVA_ARGS environment variable

--- nadeef.sh
+++ nadeef.sh
@@ -3,24 +3,35 @@ export BuildVersion='1.0.1019'
 
 type -P javac > /dev/null 2>&1 || { echo "JDK cannot be found, please check your PATH var."; exit 1; }
  
+function run_console()
+{
+  cmd="java $JAVA_ARGS -cp out/bin/*:examples/:out/test qa.qcri.nadeef.console.Console"
+  exec $cmd
+}
+
+function run_dashboard()
+{
+  cmd="java $JAVA_ARGS -cp out/bin/*:. qa.qcri.nadeef.web.NadeefStart"
+  exec $cmd
+}
+
 if ! [ -d "out" ]; then
     echo NADEEF is not yet compiled, please first run 'ant' to build it.
 else
     if [ $# -eq 1 ]; then
         if [ "$1" == "console" ]; then
-            cmd='java -cp out/bin/*:examples/:out/test qa.qcri.nadeef.console.Console'
-            exec $cmd
+          run_console
         elif [ "$1" == "dashboard" ]; then
-            cmd='java -cp out/bin/*:. qa.qcri.nadeef.web.NadeefStart'
-            exec $cmd
+          run_dashboard
         else
             echo 'Usage: nadeef.sh [OPTIONS]'
             echo 'Options are:'
             echo '    console : start the NADEEF console.'
             echo '    dashboard : start the NADEEF dashboard.'
+            echo 'Environment variables considered: JAVA_ARGS. Example: JAVA_ARGS="-Xmx15G"'
         fi
     else
-            cmd='java -cp out/bin/*:examples/:out/test qa.qcri.nadeef.console.Console'
-            exec $cmd
+      run_console
     fi
 fi
+
